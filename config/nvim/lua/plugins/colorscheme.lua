47ea7ba9b6ddbe48f20394374fa789aed2f9b032
ogierlicz <oskargierlicz@gmail.com>
2025-10-01 17:26:07 +0200

    add new themes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,28 @@
 return {
   -- add gruvbox
-  { "wittyjudge/gruvbox-material.nvim" },
+  {
+    "wittyjudge/gruvbox-material.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- add gruvdark
+  {
+    "darianmorat/gruvdark.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  -- add bamboo
+  {
+    "ribru17/bamboo.nvim",
+    lazy = false,
+    priority = 1000,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "gruvdark",
     },
   },
 }
